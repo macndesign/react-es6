89ff48392a3f5e1324898d54bfb6845d4bc00c72
Mário Chaves <macndesign@gmail.com>
2016-02-15 01:39:33 -0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Wrapper from './App2';
+import App3 from './App3';
+import App4 from './App4';
+import App5 from './App5';
+
+
+ReactDOM.render(
+  <App cat={5} />,
+  document.getElementById('app')
+);
+
+ReactDOM.render(
+  <Wrapper />,
+  document.getElementById('app2')
+);
+
+ReactDOM.render(
+  <App3 />,
+  document.getElementById('app3')
+);
+
+ReactDOM.render(
+  <App4 />,
+  document.getElementById('app4')
+);
+
+ReactDOM.render(
+  <App5 />,
+  document.getElementById('app5')
+);
